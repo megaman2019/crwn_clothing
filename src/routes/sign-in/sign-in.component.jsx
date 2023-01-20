7b31e3a9f285dc63ea7b00fd82e4f5015ad5ef1e
rowel <rowel@readysetsoftware.com.au>
2023-01-20 23:28:21 +0800

    Added Signup form and saves user into firebase

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,10 +1,28 @@
+// import { useEffect } from "react"
+// import { getRedirectResult } from "firebase/auth"
+
 import {
+  // auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
+  // signinWithGoogleRedirect,
 } from "../../utils/firebase/firebase.utils"
 
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component"
+
 const SignIn = () => {
-  const logGoogleUser = async () => {
+  // useEffect(() => {
+  //   async function fetch() {
+  //     const response = await getRedirectResult(auth)
+  //     if (response) {
+  //       const userDocRef = await createUserDocumentFromAuth(response.user)
+  //     }
+  //   }
+
+  //   fetch()
+  // }, [])
+
+  const logGooglePopupUser = async () => {
     const { user } = await signInWithGooglePopup()
     const userDocRef = await createUserDocumentFromAuth(user)
   }
@@ -12,7 +30,11 @@ const SignIn = () => {
   return (
     <div>
       <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <button onClick={logGooglePopupUser}>Sign in with Google Popup</button>
+      {/* <button onClick={signinWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button> */}
+      <SignUpForm />
     </div>
   )
 }
