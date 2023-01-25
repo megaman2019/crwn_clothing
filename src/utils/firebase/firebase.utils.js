db0756593d7fb2be9c3e58e48c9de177d7096eca
rowel <rowel@readysetsoftware.com.au>
2023-01-25 21:56:37 +0800

    Firebase Database Storage

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -10,7 +10,16 @@ import {
   onAuthStateChanged,
 } from "firebase/auth"
 
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore"
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs,
+} from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyBfJ6_qNk-lAaax-yecDtOKvvhr2jzCFwY",
@@ -37,6 +46,25 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
 
 export const db = getFirestore()
 
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = collection(db, collectionKey)
+  const batch = writeBatch(db)
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase())
+    batch.set(docRef, object)
+  })
+
+  await batch.commit()
+  console.log("Done importing categories..")
+}
+
+/**
+ * Start Authentication Methods
+ */
 export const createUserDocumentFromAuth = async (
   userAuth,
   additionalInformation = {}
@@ -82,4 +110,25 @@ export const signOutUser = async () => await signOut(auth)
 
 export const onAuthStateChangedListener = async (callback) => {
   onAuthStateChanged(auth, callback)
+}
+
+/**
+ * End Authentication Methods
+ */
+
+/**
+ * GET Categories and Documents
+ */
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, "categories")
+  const q = query(collectionRef)
+
+  const querySnapshot = await getDocs(q)
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    const { title, items } = docSnapshot.data()
+    acc[title.toLowerCase()] = items
+    return acc
+  }, {})
+
+  return categoryMap
 }
