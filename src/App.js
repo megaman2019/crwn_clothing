fa2a69001dcc1b7370e8695e04658c0870dbb510
rowel <rowel@readysetsoftware.com.au>
2023-01-26 14:14:09 +0800

    used redux on user

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
 import { Routes, Route } from "react-router-dom"
-import Navigation from "./routes/navigation/navigation.component"
 
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from "./utils/firebase/firebase.utils"
+import { setCurrentUser } from "./store/user/user.action"
+
+import Navigation from "./routes/navigation/navigation.component"
 import Home from "./routes/home/home.component"
 import Authentication from "./routes/authentication/authentication.component"
 import Shop from "./routes/shop/shop.component"
 import Checkout from "./components/checkout/checkout.component"
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user)
+      }
+      dispatch(setCurrentUser(user))
+    })
+
+    return unsubscribe
+  }, [])
+
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,5 @@
-import { useState, createContext, useEffect } from "react"
+import { createContext, useEffect, useReducer } from "react"
+import { createAction } from "../utils/reducer/reducer.utils"
 
 import {
   onAuthStateChangedListener,
@@ -11,8 +12,38 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 })
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+}
+
+const INITIAL_STATE = {
+  currentUser: null,
+}
+
+const userReducer = (state, action) => {
+  const { type, payload } = action
+  console.log("dispatched")
+  console.log(action)
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      }
+
+    default:
+      throw new Error(`Undhandled type ${type} in userReducer`)
+  }
+}
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null)
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE)
+  console.log(currentUser)
+
+  const setCurrentUser = (user) => {
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user))
+  }
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
